d07a7641d3c5d5653c8be01e7d8e1c10e6d21f0e
Mohit-24github <mohitsomani243@gmail.com>
2025-05-01 22:53:17 +0530

    Updating Author column In Blogs Page.

--- frontend/src/components/completeBlog.tsx
+++ frontend/src/components/completeBlog.tsx
@@ -20,26 +20,28 @@ export const CompleteBlog = ( {blog} :{blog: blog})=>{
                         {blog.title}
                     </div>
                     <div className="text-slate-600 pt-3">
-                        Published on 14.04.3035
+                        Published on 01.05.2025
                     </div>
                     <div className="pt-4">
                         {blog.content}
                     </div>
                 </div>
-                <div className="flex-col grid col-span-2">
-                    <div className="px-2 font-semibold text-lg">
-                        Author
-                    </div>
-                    <div className=" flex grid-cols-4">
-                        <div className="flex justify-center flex-col pr-2 col-span-1">
-                            <Avatar name={blog.author.name === null? "Anonymous": blog.author.name} size="big"/>
+                <div className="flex justify-center items-start col-span-2">
+                    <div className="rounded-lg p-6 w-80">
+                        <div className="px-2 font-semibold text-lg">
+                            Author
                         </div>
-                        <div className="col-span-3 pl-2">
-                            <div className="font-bold text-xl pt-2">
-                                {blog.author.name === null? "Anonymous": blog.author.name}
+                        <div className="flex pt-4">
+                            <div className="flex justify-center flex-col pr-4">
+                                <Avatar name={blog.author.name === null ? "Anonymous" : blog.author.name} size="big" />
                             </div>
-                            <div className="text-slate-500 pt-2">
-                                This is the place where we give the description of the author after the website is updated a little bit.
+                            <div>
+                                <div className="font-bold text-xl pt-2">
+                                    {blog.author.name === null ? "Anonymous" : blog.author.name}
+                                </div>
+                                <div className="text-slate-500 pt-2 text-sm">
+                                    This is the place where we give the description of the author after the website is updated a little bit.
+                                </div>
                             </div>
                         </div>
                     </div>
